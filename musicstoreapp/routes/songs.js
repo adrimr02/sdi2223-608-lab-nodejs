f1ec1723211535c2d02346e520798768b9008c84
Adrián Martínez <adrianmartinezr2002@gmail.com>
2023-03-20 20:26:51 +0100

    SDI-2223-608-7.4-Recursos estáticos, peticiones POST y parámetros, Enrutamiento y comodines.

--- musicstoreapp/routes/songs.js
+++ musicstoreapp/routes/songs.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
         if (req.query.title)
             response += `Título: ${req.query.title}<br>`
         if (req.query.author)
-            response += `Título: ${req.query.author}<br>`
+            response += `Autor: ${req.query.author}<br>`
 
         res.send(response);
     })
@@ -13,6 +13,7 @@ module.exports = function (app) {
         let response = 'id: ' + req.params.id;
         res.send(response);
     });
+
     app.get('/songs/:kind/:id', function(req, res) {
         let response = 'id: ' + req.params.id + '<br>'
             + 'Tipo de música: ' + req.params.kind;
@@ -23,4 +24,57 @@ module.exports = function (app) {
         let response = parseInt(req.query.num1) + parseInt(req.query.num2);
         res.send(String(response));
     });
+
+    app.post('/songs/add', function(req, res) {
+        let response = `Canción agregada: ${req.body.title}<br>`+
+            `genero: ${req.body.kind}<br>`+
+            `precio: ${req.body.price}`
+
+        res.send(response)
+    })
+
+    app.get('/promo*', function (req, res) {
+        res.send('Respuesta al patrón promo*');
+    });
+
+    app.get('/pro*ar', function (req, res) {
+        res.send('Respuesta al patrón pro*ar');
+    });
+
+    app.get('/', function (req, res) {
+        res.send('root');
+    });
+
+    app.get('/about', function (req, res) {
+        res.send('about');
+    });
+
+    app.get('/random.text', function (req, res) {
+        res.send('random.text');
+    });
+
+    app.get('/ab?cd', function(req, res) {
+        res.send('ab?cd');
+    });
+
+    app.get('/ab+cd', function(req, res) {
+        res.send('ab+cd');
+    });
+
+    app.get('/ab*cd', function(req, res) {
+        res.send('ab*cd');
+    });
+
+    app.get('/ab(cd)?e', function(req, res) {
+        res.send('ab(cd)?e');
+    });
+
+    app.get(/a/, function(req, res) {
+        res.send('/a/');
+    });
+
+    app.get(/.*fly$/, function(req, res) {
+        res.send('/.*fly$/');
+    });
+
 }
