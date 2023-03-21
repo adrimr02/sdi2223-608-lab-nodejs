db6a0efeb7da4d7e27f994e9a0333860914be2e9
Adrián Martínez <adrianmartinezr2002@gmail.com>
2023-03-21 12:12:47 +0100

    SDI–2223-608-7.101-Ejercicio complementario 1 Correccion

--- musicstoreapp/routes/authors.js
+++ musicstoreapp/routes/authors.js
@@ -83,10 +83,17 @@ module.exports = function(app) {
         let response = ''
         if (req.body.name)
             response += `Nombre: ${req.body.name}<br>`
+        else
+            response += `Nombre no enviado en la petición<br>`
         if (req.body.group)
-            response += `Autor: ${req.body.group}<br>`
+            response += `Grupo: ${req.body.group}<br>`
+        else
+            response += `Grupo no enviado en la petición<br>`
+
         if (req.body.role)
             response += `Rol: ${req.body.role}<br>`
+        else
+            response += `Rol no enviado en la petición<br>`
 
         res.send(response)
     })
--- musicstoreapp/routes/songs.js
+++ musicstoreapp/routes/songs.js
@@ -27,20 +27,10 @@ module.exports = function (app) {
 
     app.post('/songs/add', function(req, res) {
         let response = ''
-        if (req.body.name)
-            response += `Nombre: ${req.body.name}<br>`
-        else
-            response += `Nombre no enviado en la petición<br>`
-
-        if (req.body.group)
-            response += `Grupo: ${req.body.group}<br>`
-        else
-            response += `Grupo no enviado en la petición<br>`
-
-        if (req.body.role)
-            response += `Rol: ${req.body.role}<br>`
-        else
-            response += `Rol no enviado en la petición<br>`
+        if (req.body.title)
+            response += `Título: ${req.body.title}<br>`
+        if (req.body.author)
+            response += `Autor: ${req.body.author}<br>`
 
         res.send(response);
     })
